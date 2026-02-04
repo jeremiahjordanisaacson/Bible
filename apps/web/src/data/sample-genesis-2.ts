a6a1b208bc4d78de2458216244dd40f8155df874
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2026-02-04 12:35:35 -0800

    Add Genesis 2:8-14 Hebrew morphological data
    Garden of Eden, tree of life, tree of knowledge of good and evil,
    and the four rivers (Pishon, Gihon, Tigris/Hiddekel, Euphrates).

--- apps/web/src/data/sample-genesis-2.ts
+++ apps/web/src/data/sample-genesis-2.ts
@@ -4,8 +4,9 @@
  */
 
 import { genesis2_1_7_verses } from './genesis/gen-2-1-7';
+import { genesis2_8_14_verses } from './genesis/gen-2-8-14';
 
-// Genesis chapter 2 verses (starting with 2:1-7)
-export const genesisChapter2Verses = [...genesis2_1_7_verses];
+// Genesis chapter 2 verses (2:1-14)
+export const genesisChapter2Verses = [...genesis2_1_7_verses, ...genesis2_8_14_verses];
 
 export default genesisChapter2Verses;
